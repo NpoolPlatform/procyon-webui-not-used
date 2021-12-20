74a764b69dcb1962c861b55f2985541a4955cf79
crazyzpl <crazyzplzpl@163.com>
2021-12-20 11:50:00 +0800

    update package config

--- quasar.conf.js
+++ quasar.conf.js
@@ -35,7 +35,7 @@ module.exports = configure(function (ctx) {
 
     htmlVariables: {
       title: 'Procyon',
-      description: 'Japan Cloud Power --- Procyon',
+      description: 'Procyon\'s cloud power app',
       favicon: '/public/favicon.ico'
     },
 
