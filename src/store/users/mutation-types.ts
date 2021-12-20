77fa9c264c9c1e0ff4a95d85b5c254639719df36
crazyzpl <crazyzplzpl@163.com>
2021-12-20 19:57:53 +0800

    update mutation

--- src/store/users/mutation-types.ts
+++ src/store/users/mutation-types.ts
@@ -2,6 +2,6 @@ export enum MutationTypes {
   SetUserInfo = 'USER_SET_USER_INFO',
   SetUserLogined = 'USER_SET_USER_LOGINED',
   SetUserBasicInfo = 'USER_SET_USER_BASIC_INFO',
-  SetError = 'WALLET_SET_ERROR',
-  SetLoading = 'WALLET_SET_LOADING'
+  SetError = 'USER_SET_ERROR',
+  SetLoading = 'USER_SET_LOADING'
 }
