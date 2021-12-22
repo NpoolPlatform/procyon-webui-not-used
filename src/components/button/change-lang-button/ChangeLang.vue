440031087ed34989da6acd75e3981ee4e0c44ba7
crazyzpl <crazyzplzpl@163.com>
2021-12-22 11:31:26 +0800

    update header

---
+++ src/components/button/change-lang-button/ChangeLang.vue
@@ -0,0 +1,38 @@
+<template>
+  <q-btn-toggle
+    v-model="locale"
+    push
+    glossy
+    toggle-color="orange-9"
+    @click="changeLang"
+    :options="languageOptions"
+  />
+</template>
+
+<script setup lang="ts">
+import { withDefaults, defineProps, toRef } from 'vue'
+import { useStore } from 'src/store'
+import { useI18n } from 'vue-i18n'
+import { MutationTypes } from 'src/store/style/mutation-types'
+
+interface Props {
+  languageOptions?: {label: string, value: string}[]
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  languageOptions: () => [{ label: 'En', value: 'en-US' }, { label: '日本語', value: 'ja-JP' }]
+})
+
+const languageOptions = toRef(props, 'languageOptions')
+
+const store = useStore()
+const { locale } = useI18n({ useScope: 'global' })
+
+const changeLang = () => {
+  if (locale.value === 'en-US') {
+    store.commit(MutationTypes.SetFontStyle, 'font-family: Barlow')
+  } else {
+    store.commit(MutationTypes.SetFontStyle, 'font-family: "Noto Sans JP"')
+  }
+}
+</script>
--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -17,29 +17,21 @@
     <a class="nav-link" href="mailto:support@procyon.vip">
       {{ $t('header.Contact') }}
     </a>
-    <div class="button-group" style="margin-right: 10px">
-      <q-btn-toggle
-        v-model="locale"
-        push
-        glossy
-        toggle-color="orange-9"
-        @click="changeLang"
-        :options="[
-          { label: 'En', value: 'en-US' },
-          { label: '日本語', value: 'ja-JP' },
-        ]"
-      />
-    </div>
+    <ChangeLang style="margin-right: 10px"></ChangeLang>
     <q-btn
       v-if="!logined"
-      class="btn-register"
-      style="width: 140px; margin-right: 10px; text-transform: unset"
+      class="common-button register"
       flat
       @click="$router.push('/emailregister')"
     >
       {{ $t('header.Register') }}
     </q-btn>
-    <q-btn v-if="!logined" class="alt" flat @click="$router.push('/login')">
+    <q-btn
+      v-if="!logined"
+      class="common-button login"
+      flat
+      @click="$router.push('/login')"
+    >
       {{ $t('header.Login') }}
     </q-btn>
     <AvatarDropdown></AvatarDropdown>
@@ -47,40 +39,18 @@
 </template>
 
 <script setup lang="ts">
+import ChangeLang from '../button/change-lang-button/ChangeLang.vue'
 import AvatarDropdown from './dropdown/AvatarDropdown.vue'
 import logo from '../../assets/procyon-light.svg'
+
 import { useStore } from 'src/store/index'
-import { useI18n } from 'vue-i18n'
 import { computed } from 'vue'
-import { MutationTypes } from 'src/store/style/mutation-types'
 
 const store = useStore()
-// eslint-disable-next-line @typescript-eslint/unbound-method
-const { locale } = useI18n({ useScope: 'global' })
-
 const logined = computed(() => store.getters.getUserLogined)
-const changeLang = () => {
-  if (locale.value === 'en-US') {
-    store.commit(MutationTypes.SetFontStyle, 'font-family: Barlow')
-  } else {
-    store.commit(MutationTypes.SetFontStyle, 'font-family: "Noto Sans JP"')
-  }
-}
 </script>
 
 <style scoped>
-/* a:not([class]) {
-  color: #1ec498;
-  filter: contrast(1.5);
-  transition: all ease-in-out 0.3s;
-}
-
-a:not([class]):hover::after {
-  color: #1ec498;
-  filter: contrast(1.5);
-  text-decoration: underline 1px solid #11afaf;
-}
- */
 a:hover {
   color: #11afaf;
   filter: contrast(1.5);
@@ -93,7 +63,6 @@ a:hover {
 
 .header-right {
   display: flex;
-  justify-content: space-around;
   align-items: center;
   padding: 12px;
 }
@@ -115,7 +84,7 @@ a:hover {
     transparent 100%
   );
   display: block;
-  content: "";
+  content: '';
   position: absolute;
   left: 0;
   bottom: -4px;
@@ -125,24 +94,17 @@ a:hover {
   transform: scaleX(0);
 }
 
-.alt {
+.register {
+  background: linear-gradient(to bottom right, #ff964a 0, #ce5417 100%);
+  border: 0;
+  color: #e4f4f0;
+  width: 140px;
+  margin-right: 10px;
+}
+
+.login {
   background: none;
   border: 1px solid #ff964a;
   color: #ff964a;
-  text-shadow: 1px 1px 1px #27424c;
-  border-radius: 8px;
-  font-size: 14px;
-  height: auto;
-  margin: 24px 0 0 12px;
-  padding: 8px 20px;
-  border-radius: 18px;
-  cursor: pointer;
-  /* font-family: "Barlow", sans-serif; */
-  font-weight: 600;
-  transition: all ease-in-out 0.1s;
-}
-
-.alt:hover {
-  border-radius: 2px;
 }
 </style>
