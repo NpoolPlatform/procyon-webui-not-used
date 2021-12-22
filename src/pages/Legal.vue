71a4af711315662108571dfce9e32d1dfaa22058
crazyzpl <crazyzplzpl@163.com>
2021-12-22 22:02:44 +0800

    refact legal

--- src/pages/Legal.vue
+++ src/pages/Legal.vue
@@ -1,7 +1,153 @@
 <template>
-  <div></div>
+  <div id="main">
+    <div class="content legal">
+      <div id="hero">
+        <h1>{{ $t('legal.Title1') }}</h1>
+        <h2>{{ $t('legal.Title2') }}</h2>
+      </div>
+
+      <p>{{ $t('legal.P1') }}</p>
+      <h2>{{ $t('legal.H1') }}</h2>
+      <p>{{ $t('legal.P2') }}</p>
+      <p>{{ $t('legal.P3') }}</p>
+      <h2>{{ $t('legal.H2') }}</h2>
+      <p>{{ $t('legal.P4') }}</p>
+      <ul>
+        <li>{{ $t('legal.U1.L1') }}</li>
+        <li>{{ $t('legal.U1.L2') }}</li>
+        <li>{{ $t('legal.U1.L3') }}</li>
+        <li>{{ $t('legal.U1.L4') }}</li>
+      </ul>
+      <p>{{ $t('legal.P5') }}</p>
+      <h2>{{ $t('legal.H3') }}</h2>
+      <p>{{ $t('legal.P6') }}</p>
+      <h2>{{ $t('legal.H4') }}</h2>
+      <p>{{ $t('legal.P7') }}</p>
+      <h2>{{ $t('legal.H5') }}</h2>
+      <p>{{ $t('legal.P8') }}</p>
+      <ul>
+        <li>{{ $t('legal.U2.L1') }}</li>
+        <li>{{ $t('legal.U2.L2') }}</li>
+        <li>{{ $t('legal.U2.L3') }}</li>
+        <li>{{ $t('legal.U2.L4') }}</li>
+        <li>{{ $t('legal.U2.L5') }}</li>
+        <li>{{ $t('legal.U2.L6') }}</li>
+        <li>{{ $t('legal.U2.L7') }}</li>
+        <li>{{ $t('legal.U2.L8') }}</li>
+      </ul>
+      <p>{{ $t('legal.P9') }}</p>
+      <p>{{ $t('legal.P10') }}</p>
+      <h1>{{ $t('legal.H6') }}</h1>
+      <h2>{{ $t('legal.H7') }}</h2>
+      <p>{{ $t('legal.P11') }}</p>
+      <h2>{{ $t('legal.H8') }}</h2>
+      <p>{{ $t('legal.P12') }}</p>
+      <h3>{{ $t('legal.H9') }}</h3>
+      <p>{{ $t('legal.P13') }}</p>
+      <h3>{{ $t('legal.H10') }}</h3>
+      <p>{{ $t('legal.P14') }}</p>
+      <h3>{{ $t('legal.H11') }}</h3>
+      <p>{{ $t('legal.P15') }}</p>
+      <h3>{{ $t('legal.H12') }}</h3>
+      <p>{{ $t('legal.P16') }}</p>
+      <ul>
+        <li>{{ $t('legal.U3.L1') }}</li>
+        <li>{{ $t('legal.U3.L2') }}</li>
+        <li>{{ $t('legal.U3.L3') }}</li>
+        <li>{{ $t('legal.U3.L4') }}</li>
+        <li>{{ $t('legal.U3.L5') }}</li>
+        <li>{{ $t('legal.U3.L6') }}</li>
+        <li>{{ $t('legal.U3.L7') }}</li>
+        <li>{{ $t('legal.U3.L8') }}</li>
+        <li>{{ $t('legal.U3.L9') }}</li>
+        <li>{{ $t('legal.U3.L10') }}</li>
+        <li>{{ $t('legal.U3.L11') }}</li>
+        <li>{{ $t('legal.U3.L12') }}</li>
+      </ul>
+      <h3>{{ $t('legal.hh1') }}</h3>
+      <p>{{ $t('legal.pp1') }}</p>
+      <h2>{{ $t('legal.hh2') }}</h2>
+      <p>{{ $t('legal.pp2') }}</p>
+      <h2>{{ $t('legal.hh3') }}</h2>
+      <p>{{ $t('legal.pp3') }}</p>
+      <h2>{{ $t('legal.hh4') }}</h2>
+      <p>{{ $t('legal.pp4') }}</p>
+      <p>{{ $t('legal.pp5') }}</p>
+      <h2>{{ $t('legal.hh5') }}</h2>
+      <p>{{ $t('legal.pp6') }}</p>
+      <h2>{{ $t('legal.hh6') }}</h2>
+      <p>{{ $t('legal.pp7') }}</p>
+      <h2>{{ $t('legal.hh7') }}</h2>
+      <p>{{ $t('legal.pp8') }}</p>
+      <h2>{{ $t('legal.hh8') }}</h2>
+      <p>{{ $t('legal.pp9') }}</p>
+      <p>{{ $t('legal.pp10') }}</p>
+      <h2>{{ $t('legal.hh9') }}</h2>
+      <p>{{ $t('legal.pp11') }}</p>
+      <h2>{{ $t('legal.hh10') }}</h2>
+      <p>{{ $t('legal.pp12') }}</p>
+      <h2>{{ $t('legal.hh11') }}</h2>
+      <p>
+        {{ $t('legal.pp13') }}
+        <a href="mailto:support@procyon.vip">support@procyon.vip</a
+        >{{ $t('legal.pp133') }}
+      </p>
+
+      <h1>{{ $t('legal.hhh1') }}</h1>
+      <h2>{{ $t('legal.hhh2') }}</h2>
+      <p>{{ $t('legal.ppp1') }}</p>
+      <h2>{{ $t('legal.hhh3') }}</h2>
+      <p>{{ $t('legal.ppp2') }}</p>
+      <p>{{ $t('legal.ppp3') }}</p>
+
+      <h2>{{ $t('legal.he1') }}</h2>
+      <p>{{ $t('legal.pa1') }}</p>
+      <p>{{ $t('legal.pa2') }}</p>
+      <p>{{ $t('legal.pa3') }}</p>
+      <p>{{ $t('legal.pa4') }}</p>
+      <h2>{{ $t('legal.he2') }}</h2>
+      <p>{{ $t('legal.pa5') }}</p>
+      <p>{{ $t('legal.pa6') }}</p>
+      <p>{{ $t('legal.pa7') }}</p>
+      <h2>{{ $t('legal.he3') }}</h2>
+      <p>{{ $t('legal.pa8') }}</p>
+      <p>{{ $t('legal.pa9') }}</p>
+      <p>{{ $t('legal.pa10') }}</p>
+      <p>{{ $t('legal.pa11') }}</p>
+      <h2>{{ $t('legal.he4') }}</h2>
+      <p>{{ $t('legal.pa12') }}</p>
+      <p>{{ $t('legal.pa13') }}</p>
+      <p>{{ $t('legal.pa14') }}</p>
+      <h2>{{ $t('legal.he5') }}</h2>
+      <p>{{ $t('legal.pa15') }}</p>
+      <p>{{ $t('legal.pa16') }}</p>
+      <p>{{ $t('legal.pa17') }}</p>
+      <h2>{{ $t('legal.he6') }}</h2>
+      <p>{{ $t('legal.pa18') }}</p>
+      <p>{{ $t('legal.pa19') }}</p>
+
+      <div class="hr"></div>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts"></script>
 
-<style scoped></style>
+<style scoped>
+.legal h2:not(#hero h2) {
+  text-transform: unset;
+}
+
+.legal h3 {
+  background: linear-gradient(to right, #e1eeef 0, #e4f4f0 100%);
+  background-clip: border-box;
+  font-size: 24px;
+  font-weight: 400;
+  position: relative;
+  padding: 0 0 8px 0;
+  text-transform: unset;
+  -webkit-background-clip: text;
+  -webkit-box-decoration-break: clone;
+  -webkit-text-fill-color: transparent;
+}
+</style>
