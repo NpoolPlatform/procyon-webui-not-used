ba65fc29cc0964a5d11bdd0cf5d7a1882aa9944f
crazyzpl <crazyzplzpl@163.com>
2021-12-23 14:43:14 +0800

    change timestamp format function

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,44 +1,50 @@
+import dayjs from 'dayjs'
+require('dayjs/locale/ja')
+
 type timestampDigits = 2 | 3 | 0
 
 export const TimeStampToDate = (
   timestamp: string,
-  digit: timestampDigits
+  digit?: timestampDigits,
+  lang?: string
 ): string => {
-  const date = new Date(Number(timestamp) * 1000)
-  const year = date.getFullYear()
-  const month =
-    date.getMonth() + 1 < 10
-      ? '0' + (date.getMonth() + 1).toString()
-      : date.getMonth() + 1
-  const day = date.getDate()
-  const hour = date.getHours()
-  const minute = date.getMinutes()
-  const second = date.getSeconds()
-
-  let formatTime = ''
-
-  switch (digit) {
-    case 2:
-      formatTime = year.toString() + '.' + month.toString()
-      break
-    case 3:
-      formatTime =
-        year.toString() + '.' + month.toString() + '.' + day.toString()
+  let date: dayjs.Dayjs
+  switch (lang) {
+    case 'en':
+      date = dayjs.unix(Number(timestamp))
       break
     default:
-      formatTime =
-        year.toString() +
-        '.' +
-        month.toString() +
-        '.' +
-        day.toString() +
-        ' ' +
-        hour.toString() +
-        ':' +
-        minute.toString() +
-        ':' +
-        second.toString()
+      date = dayjs.unix(Number(timestamp)).locale('ja')
       break
   }
-  return formatTime
+
+  let formatDate: string
+  switch (digit) {
+    case 2:
+      formatDate = date.get('y').toString() + '.' + date.get('month').toString()
+      break
+    case 3:
+      formatDate =
+        date.get('y').toString() +
+        '.' +
+        date.get('month').toString() +
+        date.get('date').toString()
+      break
+    default:
+      formatDate =
+        date.get('y').toString() +
+        '.' +
+        date.get('month').toString() +
+        '.' +
+        date.get('date').toString() +
+        ' ' +
+        date.get('hour').toString() +
+        ':' +
+        date.get('minute').toString() +
+        ':' +
+        date.get('second').toString()
+      break
+  }
+
+  return formatDate
 }
