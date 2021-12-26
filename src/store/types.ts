55b00affd1e1bffb2ef16f92d9906f582bc404e9
crazyzpl <crazyzplzpl@163.com>
2021-12-26 18:18:57 +0800

    commit store types

--- src/store/types.ts
+++ src/store/types.ts
@@ -1,5 +1,14 @@
-export interface NotifyMessage {
-  wait: string
-  success: string
-  fail: string
+import { notifyType } from 'src/notify/notify'
+
+export interface allMessages {
+  loadingMessage: string
+  successMessage: string
+  failMessage: string
+  error: string
+  type: notifyType
+}
+
+export interface RequestInput<T> {
+  requestInput: T,
+  messages: allMessages
 }
