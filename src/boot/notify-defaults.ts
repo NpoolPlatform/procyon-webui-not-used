0afd833d7dbf8b7254fa888ea48446f2de4055f1
Zhao KK <kikakkz@hotmail.com>
2022-01-05 01:09:19 +0800

    Hint to bottom right

--- src/boot/notify-defaults.ts
+++ src/boot/notify-defaults.ts
@@ -4,7 +4,7 @@ import { Notify } from 'quasar'
 export default boot((/* { app, router, ... } */) => {
   Notify.setDefaults({
     color: 'white',
-    timeout: 3000,
-    position: 'top-right'
+    timeout: 5000,
+    position: 'bottom-right'
   })
 })
