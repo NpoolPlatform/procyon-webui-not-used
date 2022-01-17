1bc08869932ba5fb29505f9801e16751644d12e1
Zhao KK <kikakkz@hotmail.com>
2022-01-17 21:38:12 +0800

    Use new api

--- src/store/kycs/const.ts
+++ src/store/kycs/const.ts
@@ -5,7 +5,7 @@ enum ImageType {
 }
 
 enum API {
-  GET_KYC_INFO = '/cloud-hashing-apis-v2/v1/get/kyc/by/user',
+  GET_KYC_INFO = '/cloud-hashing-apis-v2/v1/get/kyc/by/app/user',
   GET_KYC_IMAGE = '/kyc-management/v1/get/kyc/image',
   UPLOAD_KYC_IMAGE = '/kyc-management/v1/upload/kyc/image',
   CREATE_KYC = '/cloud-hashing-apis-v2/v1/create/kyc'
