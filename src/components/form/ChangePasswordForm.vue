1e7dc6b0341e38678a3975e3f555dee370eee581
Zhao KK <kikakkz@hotmail.com>
2022-03-28 15:22:10 +0800

    Goto dashboard when successful

--- src/components/form/ChangePasswordForm.vue
+++ src/components/form/ChangePasswordForm.vue
@@ -69,7 +69,6 @@ import { useStore } from 'src/store'
 import { ItemStateTarget } from 'src/store/types'
 import { ActionTypes } from 'src/store/users/action-types'
 import { UserUpdatePasswordByAppUserRequest } from 'src/store/users/types'
-import { useRouter } from 'src/router'
 
 const SendCodeInput = defineAsyncComponent(() => import('src/components/input/SendCodeInput.vue'))
 // eslint-disable-next-line @typescript-eslint/unbound-method
@@ -112,8 +111,6 @@ const oldPasswordRules = ref([
   (val: string) => (val && val.length > 0) || t('input.OldPasswordWarning')
 ])
 
-const router = useRouter()
-
 const changePassword = throttle(() => {
   let type = ''
   let verifyParam = ''
@@ -133,8 +130,6 @@ const changePassword = throttle(() => {
     VerificationCode: verifyCode.value
   }
   store.dispatch(ActionTypes.UserChangePassword, request)
-
-  void router.push({ path: '/dashboard' })
 }, ThrottleDelay)
 </script>
 
